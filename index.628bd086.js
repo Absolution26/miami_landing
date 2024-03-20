4f0a5936a635a87ae6e57c8058538cff3abaab8b
Absolution26 <zet2571428@gmail.com>
2024-03-20 17:07:05 +0200

    make build

---
+++ index.628bd086.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#navigation"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});var e=document.getElementsByClassName("form");e[0].addEventListener("submit",function(n){n.preventDefault(),e[0].reset()});
+//# sourceMappingURL=index.628bd086.js.map
